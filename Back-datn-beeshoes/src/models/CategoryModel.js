98baf976e8949128b8c1b820e646e437f1cec43f
Liem <ngotrongliem2004@gmail.com>
2025-03-21 19:41:47 +0700

    update category models success

--- Back-datn-beeshoes/src/models/CategoryModel.js
+++ Back-datn-beeshoes/src/models/CategoryModel.js
@@ -1,13 +1,7 @@
 const mongoose = require('mongoose');
 
-const categorySchema = new mongoose.Schema(
-    {
-        name: { type: String, required: true }
-    },
-    {
-        timestamps: true
-    }
-);
+const CategorySchema = new mongoose.Schema({
+    name: { type: String, required: true }
+}, { timestamps: true });
 
-const Category = mongoose.model('Category', categorySchema);
-module.exports = Category;
+module.exports = mongoose.model('Category', CategorySchema);
